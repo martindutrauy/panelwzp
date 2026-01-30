4f02987c50a9f46cd81b7e8f07b3d34da20e196f
Gonzalo Fernandez <gonzalofernandez2013.gf@gmail.com>
2026-01-30 12:27:16 -0300

    desploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        {
+            name: 'serve-index-html-at-root',
+            configureServer(server) {
+                server.middlewares.use((req, _res, next) => {
+                    if (req.url === '/' || req.url === '') req.url = '/index.html'
+                    next()
+                })
+            }
+        }
+    ],
     server: {
         port: 3000,
         proxy: {
